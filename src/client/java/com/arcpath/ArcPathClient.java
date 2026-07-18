04c8121a77743e0f9f14da998e26b7f9fb1f1bfe
Ryan Kirsch <ryankirschcontact@gmail.com>
2026-07-17 22:39:03 -0500

    Clear debug history when player switches to a non-throwable

--- src/client/java/com/arcpath/ArcPathClient.java
+++ src/client/java/com/arcpath/ArcPathClient.java
@@ -3,6 +3,7 @@ package com.arcpath;
 import com.arcpath.config.ArcPathConfig;
 import com.arcpath.input.ArcPathKeyBinds;
 import com.arcpath.trajectory.DebugRenderer;
+import com.arcpath.trajectory.ProjectileType;
 import com.arcpath.trajectory.TrajectoryRenderer;
 
 import net.fabricmc.api.ClientModInitializer;
@@ -26,6 +27,14 @@ public class ArcPathClient implements ClientModInitializer {
         DebugRenderer.register();
 
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            if (client.player == null) 
+                return;
+
+            // clear debug history when player switches to a non-throwable
+            var heldItem = client.player.getMainHandItem();
+            if (ProjectileType.fromItem(heldItem.getItem()).isEmpty())
+                DebugRenderer.clear();
+
             while (ArcPathKeyBinds.TOGGLE_ARC.consumeClick()) {
                 ArcPathState.toggleEnabled();
                 // clear debug lines when arc is toggled off
